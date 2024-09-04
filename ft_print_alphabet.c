5498c37f2d8f462eafe0a8a3e8b58aaf5f06a52c
AdriellyN <129234896+AdriellyN@users.noreply.github.com>
2024-09-04 12:11:32 -0300

    C00
    Exercícios de 0 a 4. Lista 0.

---
+++ ft_print_alphabet.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ado-nasc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/20 16:58:08 by ado-nasc          #+#    #+#             */
+/*   Updated: 2024/08/21 17:55:53 by ado-nasc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	alpha;
+
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		write(1, &alpha, 1);
+		alpha++;
+	}
+}
+
+/*
+int     main(void)
+{
+        ft_print_alphabet();
+        return (0);
+}
+*/
---
+++ ft_print_reverse_alphabet.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ado-nasc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/20 17:28:14 by ado-nasc          #+#    #+#             */
+/*   Updated: 2024/08/21 17:59:12 by ado-nasc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	alpha;
+
+	alpha = 'z';
+	while (alpha >= 'a')
+	{
+		write(1, &alpha, 1);
+		alpha--;
+	}
+}
+
+/*
+int     main(void)
+{
+        ft_print_reverse_alphabet();
+        return (0);
+}
+*/
